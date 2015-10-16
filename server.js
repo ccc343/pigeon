fcaadb0b81e4af7439958f3c2fb1b163ffa3f8f0
Julia Wang <juliahw@princeton.edu>
2015-10-16 19:43:18 -0400

    Initial setup

---
+++ server.js
@@ -0,0 +1,22 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/build', express.static(__dirname + '/build'));
+
+app.get('/', function (req, res) {
+  res.render('application.garnet');
+});
+
+app.post('/subscribe', function (req, res) {
+  console.log(req.body);
+  res.json({
+    success: true
+  });
+});
+
+var server = app.listen(5000, function() {
+  console.log('Listening on port %d.', server.address().port);
+});
---
+++ views/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.displayName = 'Index';
+  }
+
+  render() {
+    return <div>I realllly like pie!</div>;
+  }
+}
+
+ReactDOM.render(React.createElement(Index), document.getElementById('react-root'));
