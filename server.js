be4216041eac9cda5e7a66c29fab04aa1d8fe9ed
Julia Wang <juliahw@princeton.edu>
2015-10-18 19:55:39 -0400

    Code cleanup

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var db = require('./db');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/build', express.static(__dirname + '/build'));
 
+// API end points
 app.get('/', function (req, res) {
+  db.test();
   res.render('application.garnet');
-});
-
-app.post('/subscribe', function (req, res) {
-  console.log(req.body);
-  res.json({
-    success: true
-  });
 });
 
 var server = app.listen(process.env.PORT || 5000, function() {
